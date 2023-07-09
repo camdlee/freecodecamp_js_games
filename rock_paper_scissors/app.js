691f2bbe2685f6b236bdd750b4e8ccbb1210d9e9
Cameron Lee <camdlee18@gmail.com>
2023-07-09 17:26:06 -0400

    initialized variables in js, addEventListener for user choice

---
+++ rock_paper_scissors/app.js
@@ -0,0 +1,12 @@
+
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice 
+
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event)=>{
+    userChoice = event.target.id
+    userChoiceDisplay.innerHTML = userChoice
+}))

0a28a64f09f5413cc3a3d0ba8d38ff3b0fe0c503
Cameron Lee <camdlee18@gmail.com>
2023-07-09 17:34:34 -0400

    added generate computer choice

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -4,9 +4,26 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice 
+let computerChoice
 
 
+// Your Choice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event)=>{
     userChoice = event.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+// Function to generate Computer Choice
+const generateComputerChoice = () => {
+    const randomNum = Math.floor(Math.random() * possibleChoices.length) + 1
+    // console.log(randomNum)
+    if(randomNum === 1){
+        computerChoice = 'rock'
+    } else if (randomNum === 2) {
+        computerChoice = 'paper'
+    } else {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}

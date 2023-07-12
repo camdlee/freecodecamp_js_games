0b8ae370dd49dae6a28fe10ccab488574c7b3ce0
Cameron Lee <camdlee18@gmail.com>
2023-07-11 20:08:26 -0400

    adding score calculation

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -5,6 +5,7 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let moleSquare
 
 // Function to randomize where mole is located in grid
 const randomSquare = () =>{
@@ -17,8 +18,30 @@ const randomSquare = () =>{
     // math.random will retrun a random value between 0 and 1, 0 is possible but not 1
     // Multiplying by 9 will get us close to any of the whole numbers for our squares
     // math.floor will round down that float
-    let randomPosition = squares[Math.floor(Math.random()*9)]
-    console.log(randomPosition)
+    let randomSquare = squares[Math.floor(Math.random()*9)]
+    randomSquare.classList.add('mole')
+
+    moleSquare = randomSquare.id
 }
 
-randomSquare()
+// Function to calculate score
+squares.forEach(square=> {
+    // adding an event listener where for each square it will check to see if the mouse is in the same square as the mole
+    square.addEventListener('movedown', ()=>{
+        if (square.id == moleSquare){
+            result++
+            score.textContent = result
+            moleSquare = null
+        }
+    })
+})
+
+
+// Function to move mole on a timer
+const moveMole = () => {
+    let timerId = null
+    // setInterval will call the function in the 1st argument, 2nd argument is time in ms
+    timerId = setInterval(randomSquare, 1000)
+}
+
+// moveMole()
